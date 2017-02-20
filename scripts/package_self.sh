14e2639ea9fbba227240d16313306073d5743af7
Jonas Weber <github@jonasw.de>
2017-02-20 09:19:46 +0100

    Update package_self.sh

--- scripts/package_self.sh
+++ scripts/package_self.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd $(dirname $0)/../
+
+./upack \
+  -H Package:upack \
+  -H Version:$(git describe --always) \
+  -H "Maintainer: Jonas Weber <dev@jonasw.de>" \
+  -H Architecture:any \
+  --depends-on dash \
+  upack:/usr/bin/upack \
+  upack.1:/usr/share/man/man1/upack.1
